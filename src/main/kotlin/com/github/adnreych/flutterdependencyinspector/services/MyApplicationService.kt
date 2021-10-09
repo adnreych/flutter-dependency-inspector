2cecf9392640e8fcb97fa1c5ca31cb5cd51dd2a4
GitHub Action <action@github.com>
2021-10-09 17:43:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/adnreych/flutterdependencyinspector/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.adnreych.flutterdependencyinspector.services
+
+import com.github.adnreych.flutterdependencyinspector.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
