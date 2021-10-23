5288e3b606f6a4a96de03aa24b55e9dc46543786
adnreych <adnreych@gmail.com>
2021-10-23 20:57:40 +0300

    search files by extension

--- src/main/kotlin/com/github/adnreych/flutterdependencyinspector/actions/StartInspectionAction.kt
+++ src/main/kotlin/com/github/adnreych/flutterdependencyinspector/actions/StartInspectionAction.kt
@@ -3,15 +3,19 @@ package com.github.adnreych.flutterdependencyinspector.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.search.GlobalSearchScope
 
 
 class StartInspectionAction : AnAction() {
     override fun actionPerformed(actionEvent: AnActionEvent) {
         val project = actionEvent.project
-
+        val allFilesByExt = FilenameIndex.getAllFilesByExt(
+                project!!, "kt", GlobalSearchScope.projectScope(project))
+        println("allFilesByExt $allFilesByExt")
         Messages.showMessageDialog(
                 project,
-                "Hello world!",
+                "allFilesByExt $allFilesByExt",
                 "Greeting",
                 Messages.getInformationIcon()
         )
